6e20bcf695cbcf6a783a800fc4a32d47248e2046
Anshul Jindal <77500664+anshul-iiitb16@users.noreply.github.com>
2022-11-17 20:04:56 +0530

    Uploaded Failing test cases

---
+++ Fail_Unit_Test.py
@@ -0,0 +1,17 @@
+import unittest
+from Sum_Nat_Nums import sum_natnums
+
+class TestCase(unittest.TestCase):
+    def testcases_fail(self):
+
+        data_fail = [-2, 5]
+
+        result_fail_1 = sum_natnums(data_fail[0])
+        result_fail_2 = sum_natnums(data_fail[1])
+
+        self.assertEqual(result_fail_1, 1)
+        self.assertEqual(result_fail_2, 20)
+
+
+if __name__ == '__main__':
+    unittest.main()
