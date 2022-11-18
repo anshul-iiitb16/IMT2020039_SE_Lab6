6fb0550b53006b06151113025ef7a2d40dc24256
Anshul Jindal <77500664+anshul-iiitb16@users.noreply.github.com>
2022-11-18 13:39:19 +0530

    Updated Test cases

--- Fail_Unit_Test.py
+++ Fail_Unit_Test.py
@@ -4,15 +4,17 @@ import unittest
 from Sum_Nat_Nums import sum_natnums
 
 class TestCase(unittest.TestCase):
-    def testcases_fail(self):
+    def testcases_fail_1(self):
 
-        data_fail = [-2, 5]
-
+        data_fail = [-2]
         result_fail_1 = sum_natnums(data_fail[0])
-        result_fail_2 = sum_natnums(data_fail[1])
-
         self.assertEqual(result_fail_1, 1)
-        self.assertEqual(result_fail_2, 20)
+
+    def testcases_fail_2(self):
+
+        data_fail = [5]
+        result_fail_2 = sum_natnums(data_fail[0])
+        self.assertEqual(result_fail_2, 500)
 
 
 if __name__ == '__main__':
--- Pass_Unit_Test.py
+++ Pass_Unit_Test.py
@@ -4,16 +4,20 @@ import unittest
 from Sum_Nat_Nums import sum_natnums
 
 class TestCase(unittest.TestCase):
-    def testcases_pass(self):
+    def testcases_pass_1(self):
 
-        data_pass = [1, 5, 10]
-
+        data_pass = [1]
         result_pass_1 = sum_natnums(data_pass[0])
-        result_pass_2 = sum_natnums(data_pass[1])
-        result_pass_3 = sum_natnums(data_pass[2])
-
         self.assertEqual(result_pass_1, 1)
+
+    def testcases_pass_2(self):
+        data_pass = [5]
+        result_pass_2 = sum_natnums(data_pass[0])
         self.assertEqual(result_pass_2, 15)
+
+    def testcases_pass_3(self):
+        data_pass = [10]
+        result_pass_3 = sum_natnums(data_pass[0])
         self.assertEqual(result_pass_3, 55)
 
 
